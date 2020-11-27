e83f69a16f1460a2c43388e5ece4c55452a68238
Kish29 <875691208@qq.com>
2020-11-27 16:02:40 +0800

    2020-11-27

---
+++ src/main/java/com/kish2/hermitcrabapi/HermitcrabapiApplication.java
@@ -0,0 +1,13 @@
+package com.kish2.hermitcrabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermitcrabapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermitcrabapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kish2/hermitcrabapi/HermitcrabapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kish2.hermitcrabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermitcrabapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
